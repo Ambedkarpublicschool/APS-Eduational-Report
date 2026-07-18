93b475d909ca6795f13a7dfcf90a9e2b0bfa4f2d
Ambedkarpublicschool <apsmadhopur2024@gmail.com>
2026-07-18 19:13:10 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+
+    API_URL: "https://script.google.com/macros/s/AKfycby-_7Wu-p5W4iwKDTRHNXCcFUD3fxiX5RhtVxD3hsZIOszDhZlBwxc5Ay2OLuwDeYba1g/exec",
+
+    SCHOOL_NAME: "Aryavart Public School"
+
+};
