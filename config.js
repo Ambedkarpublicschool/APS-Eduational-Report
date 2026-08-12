2cb5b932777cddea70616e70b5c63052ce95a784
Ambedkarpublicschool <apsmadhopur2024@gmail.com>
2026-08-12 10:27:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
 
-    API_URL: "https://script.google.com/macros/s/AKfycbwuofWH1EhyN0zaBLP8vPFxowQd8ShEPVxj0z6n2nTxCKih_mKmpfxbn9rvMbWpNuVLlg/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbx0pWcvrUYEk46ArB7GiPkQGkk8LwlKeN0gtYvXwcAi7a2IKQPC3SqYMJqw5tVS0ttcOQ/exec",
 
     SCHOOL_NAME: "Ambedkar Public School"
 
